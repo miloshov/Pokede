5bfde36c0ea1935ff838c2dcbc8820363bcbeb8d
Milos Hovjecki <miloshov84@gmail.com>
2017-05-26 06:16:32 +0200

    - Folder Structure - Collection View - Search Bar - Keyboard Activity - Launch screen - Assets - Audio Player

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,35 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Milos Hovjecki on 5/25/17.
+//  Copyright © 2017 hanacode.swd. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell (_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+        
+    }
+    
+}
