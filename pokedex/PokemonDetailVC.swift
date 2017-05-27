f47d2345474b08583da83a17860ee3b012f6a3c0
Milos Hovjecki <miloshov84@gmail.com>
2017-05-27 02:56:34 +0200

    - New ViewController added - Segue was connected - When you click on one Pokemon new VC shows

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,25 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Milos Hovjecki on 5/27/17.
+//  Copyright © 2017 hanacode.swd. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    var pokemon: Pokemon!
+
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLbl.text = pokemon.name.capitalized
+
+    }
+
+
+}
--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -119,8 +119,19 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        var poke: Pokemon!
         
+        if inSearchMode {
+            
+            poke = filteredPokemon[indexPath.row]
+            
+        } else {
+            
+            poke = pokemon[indexPath.row]
+            
+        }
         
+        performSegue(withIdentifier: "PokemonDetailVC", sender: poke)
         
     }
 
@@ -191,7 +202,23 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
     }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "PokemonDetailVC" {
+            
+            if let detailsVC = segue.destination as? PokemonDetailVC {
+                
+                if let poke = sender as? Pokemon {
+                 
+                    detailsVC.pokemon = poke
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
     
     
     
